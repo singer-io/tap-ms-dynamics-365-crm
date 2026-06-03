a301441d164d1be66a54d0bf3c4722614297390d
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-06-03 18:02:31 +0530

    update cread

--- tests/base.py
+++ tests/base.py
@@ -1647,9 +1647,9 @@ class MSDynamics365CRMBaseTest(BaseCase):
             'auth_method': 'TAP_MS_DYNAMICS_365_CRM_AUTH_METHOD',
             'client_id': 'TAP_MS_DYNAMICS_365_CRM_CLIENT_ID',
             'client_secret': 'TAP_MS_DYNAMICS_365_CRM_CLIENT_SECRET',
-            'organization_uri': 'TAP_MS_DYNAMICS_365_CRM_ORGANIZATION_URI',
+            'organization_uri': 'TAP_TESTER_QLIK_REDIRECT_URI',
             'tenant_id': 'TAP_MS_DYNAMICS_365_CRM_TENANT_ID',
-            'refresh_token': 'TAP_TESTER_QLIK_REDIRECT_URI',
+            'refresh_token': 'TAP_MS_DYNAMICS_365_CRM_REFRESH_TOKEN',
         }
 
         for cred in creds:
