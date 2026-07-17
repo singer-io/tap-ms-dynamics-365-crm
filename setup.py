124e30bc6265dde45434754fece56fed88811709
RushiT0122 <rushikesh.todkar@qlik.com>
2026-07-17 15:17:06 +0530

    * Remove JSON validator from cci config

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name="tap-ms-dynamics-365-crm",
         "singer-python==6.8.0",
         "requests==2.34.2",
         "backoff==2.2.1",
-        "msal==1.34.0"
+        "msal==1.37.0"
       ],
       entry_points="""
           [console_scripts]
